c9b94ce143e344e35f4b6165a263a99386a98c79
Saman <samanaba1996@gmail.com>
2023-10-01 23:05:36 +0330

    Initial commit

---
+++ src/RayTracing.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+const int image_height = 256;
+const int image_width = 256;
+
+int main()
+{
+    std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+
+    for (int j = image_width - 1; j >= 0 ; --j)
+    {
+        for (int i = 0; i < image_height; ++i)
+        {
+            auto r = double(i) / (image_height - 1);
+            auto g = double(j) / (image_width - 1);
+            auto b = 0.25;
+
+            int ir = static_cast<int>(255.999 * r);
+            int ig = static_cast<int>(255.999 * g);
+            int ib = static_cast<int>(255.999 * b);
+
+            std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+        }
+    }
+
+    std::cout << "Hello World!\n";
+}
+
