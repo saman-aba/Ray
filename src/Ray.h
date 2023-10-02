d97815afe2e461ffea8dedf668b5ffc1119abf60
Saman <samanaba1996@gmail.com>
2023-10-02 23:49:16 +0330

    implementation of Ray class

---
+++ src/Ray.h
@@ -0,0 +1,22 @@
+#ifndef RAY_H
+#define RAY_H
+
+#include "Vec3.h"
+
+class Ray
+{
+public:
+	Ray();
+	Ray(const Point3&, const Vec3&);
+
+	Point3 origin() const;
+	Vec3 direction() const;
+
+	Point3 at(double t) const;
+
+public:
+	Point3 _origin;
+	Vec3 _direction;
+
+};
+#endif
